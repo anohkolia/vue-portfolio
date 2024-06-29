7303303973418af4599787dd1001d979664035f1
kanoh <you@example.com>
2024-06-30 01:17:35 +0200

    Mise à jour des diff dossier du projet et implémentation de la barre de navigation

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,37 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name: 'App',
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header id="header">
+    <!--Barre de navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger mt-auto" href="#page-top">
+        <span class="d-block d-lg-none d-flex align-items-center pe-2"><img class="img-fluid profile-mobile" src="/src/assets/profile.jpg" alt="Profile"></span>
+        <span class="d-block d-lg-none">Kolia ANOH</span>
+        <span class="d-none d-lg-block"><img class="img-fluid img-profile mb-2" src="/src/assets/profile.jpg" alt="Profile"></span>
+      </a>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+      <!--Bouton de navigation pour les petits écrans-->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+      aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav">
+          <li class="ms-5"><a class="align-items-center d-flex gap-2  nav-link js-scroll-trigger" href="#about"><i class="bi bi-person fs-3"></i> À propos</a></li>
+                    <li class="ms-5"><a class="align-items-center d-flex gap-2 nav-link js-scroll-trigger" href="#experience"><i class="bi bi-patch-check fs-4"></i> Expérience</a></li>
+                    <li class="ms-5"><a class="align-items-center d-flex gap-2 nav-link js-scroll-trigger" href="#formation"><i class="bi bi-journals fs-4"></i> Formation</a></li>
+                    <li class="ms-5"><a class="align-items-center d-flex gap-2 nav-link js-scroll-trigger" href="#skills"><i class="bi bi-stars fs-4"></i> Compétences</a></li>
+                    <li class="ms-5"><a class="align-items-center d-flex gap-2 nav-link js-scroll-trigger" href="#hobbie"><i class="bi bi-emoji-smile fs-4"></i> Hobbies</a></li>
+        </ul>
+      </div>
+    </nav>
   </header>
-
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
